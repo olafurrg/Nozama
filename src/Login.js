4b7c9bf97bcdda9e30f0a6ced1d7cefb0bed83a3
olafurrg <olafurg16@ru.is>
2021-03-25 16:06:32 +0000

    login and registry connected to firebase db

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import './Styling/Login.css';
+import {auth} from './firebase';
 
 function Login(){
-    
+    const history = useHistory();
+    const[userEmail, setUserEmail] = useState('');
+    const[userPassword, setUserPassword] = useState('');
+
+    const loginUser = event => {
+        event.preventDefault()
+        auth.signInWithEmailAndPassword(userEmail,userPassword)
+        .then((auth) => {
+            history.push('/')
+        })
+        .catch(e => alert(e.message))
+    }
+
+    const registerUser = event => {
+        event.preventDefault()
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
+        .then(auth => {
+            history.push('/')
+        })
+        .catch(e => alert(e.message))
+    }
+
     return (
         <div className="login">
             <Link>
@@ -13,13 +35,13 @@ function Login(){
                 <h1>Sign In</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="email" />
+                    <input value={userEmail} onChange={event => setUserEmail(event.target.value)} type="email" />
                     <h5>Password</h5>
-                    <input type="password"/>
-                    <button type="submit" className="login__button">Sign In</button>
+                    <input value={userPassword} onChange={event => setUserPassword(event.target.value)} type="password"/>
+                    <button onClick={loginUser} type="submit" className="login__button">Sign In</button>
                 </form>
                 <p>By signing-in, you agree to Nozama's Terms and Conditions</p>
-                <button className="login__button">Create your Nozama Account</button>
+                <button onClick={registerUser} className="login__button">Create your Nozama Account</button>
 
             </div>
         </div>
