a8acc3e2e5ead9db1fca6548712461c67a391381
olafurrg <olafurg16@ru.is>
2021-03-25 13:35:50 +0000

    Home page mockup

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
 import './Styling/Home.css';
+import Product from './Product';
 
 function Home(){
     return(
-        <h1>Welcome to Nozama</h1>
+        <div className="home">
+            <img className="home__image" src="https:images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
+            <div className="home__row">
+                <Product
+                    id="1234"
+                    title="The Lean Startup: How Contant Innovation"
+                    price={11.96}
+                    rating={4}
+                    image="https:images-eu.ssl-images-amazon.com/images/I/71K7Q4FpguL._AC_SL1500_.jpg"
+                />
+                <Product
+                    id="1234"
+                    title="The Lean Startup: How Contant Innovation"
+                    price={101.60}
+                    rating={3}
+                    image="https:images-eu.ssl-images-amazon.com/images/I/71K7Q4FpguL._AC_SL1500_.jpg"
+                />
+                <Product
+                    id="1234"
+                    title="The Lean Startup: How Contant Innovation"
+                    price={15.95}
+                    rating={5}
+                    image="https:images-eu.ssl-images-amazon.com/images/I/71K7Q4FpguL._AC_SL1500_.jpg"
+                />
+            </div>
+        </div>
     )
 }
 
